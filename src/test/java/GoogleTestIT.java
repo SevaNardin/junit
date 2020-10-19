118656abde00326a05bc04001abe199fe688c5e0
seva <vnardin@yandex.ru>
2020-10-19 21:15:22 +0300

    Test

--- src/test/java/GoogleTestIT.java
+++ src/test/java/GoogleTestIT.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -23,6 +24,15 @@ public class GoogleTestIT {
         String currentURL = url();
         // проверка страницы
         assertEquals( "https://www.google.com/?gws_rd=ssl", currentURL, "Error: No correct URl!" );
+    }
+
+    @Test
+    void test01() {
+        int a = 2 ;
+        int b = 4 ;
+        int c = b / a ;
+        // проверка что a = 2
+        Assertions.assertEquals( 2, a , "Error: no correct value!");
     }
 
 }
