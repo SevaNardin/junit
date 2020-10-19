d0ac4d22601236376c7c98f446d5a135f0f205ea
seva <vnardin@yandex.ru>
2020-10-19 20:08:44 +0300

    Test

--- src/test/java/GoogleTestIT.java
+++ src/test/java/GoogleTestIT.java
@@ -2,6 +2,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.sleep;
+import static com.codeborne.selenide.WebDriverRunner.url;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author Seva Nardin
@@ -12,7 +15,14 @@ public class GoogleTestIT {
     @Test
     @DisplayName( "Open google (UI Test)" )
     void openGoogle() {
+        // открытие страницы
         open("http:google.com");
+        // ожидание
+        sleep(3000);
+        // получаем текущий URL
+        String currentURL = url();
+        // проверка страницы
+        assertEquals( "https://www.google.com/?gws_rd=ssl", currentURL, "Error: No correct URl!" );
     }
 
 }
