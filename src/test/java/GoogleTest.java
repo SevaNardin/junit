2823a9be97ed90c703334d944f3d8806ef44b13e
seva <vnardin@yandex.ru>
2020-10-20 14:52:14 +0300

    Test

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -1,4 +1,5 @@
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -6,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.sleep;
 import static com.codeborne.selenide.WebDriverRunner.url;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author Seva Nardin
@@ -18,8 +18,9 @@ public class GoogleTest {
 
     @BeforeEach
     void init() {
+        // Конфигурация Selenide
         Configuration.browserSize = "1920x1080";
-        Configuration.headless = true;              // Запуск браузера заблокирован, скрытый режим
+        Configuration.headless = true;              // Запуск браузера заблокирована, скрытый режим
         Configuration.startMaximized = false;
         Configuration.holdBrowserOpen = false;
     }
@@ -34,7 +35,7 @@ public class GoogleTest {
         // получаем текущий URL
         String currentURL = url();
         // проверка страницы
-        assertEquals( "https://www.google.com/?gws_rd=ssl", currentURL, "Error: No correct URl!" );
+        Assertions.assertEquals("https://www.google.com/?gws_rd=ssl", currentURL, "Error: No correct URl!");
     }
 
 }
