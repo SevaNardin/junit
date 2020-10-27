a9613fad0151a472a373d1df20207501485c5372
seva <vnardin@yandex.ru>
2020-10-27 09:33:49 +0300

    Добавил урок

---
+++ src/main/java/Multithreading/Lesson3.java
@@ -0,0 +1,57 @@
+package Multithreading;
+
+/**
+ * @author Seva Nardin
+ */
+public class Lesson3 {
+    private int counter;
+
+    public static void main(String[] args) throws InterruptedException {
+        Lesson3 lesson3 = new Lesson3();
+        lesson3.doWork();
+    }
+    // синхронизация потоков/ у всех объектов есть монитор, завладеть монитором может только один поток
+    // всегда будет 20000 / поток2 ждет пока не отработает поток 1 и  так далее
+    public synchronized void increment() {
+        counter++;
+    }
+
+    public void doWork() throws InterruptedException {
+        // поток 1
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for( int i = 0; i < 10000; i++ ) {
+                    increment();
+                    //counter++;
+                }
+            }
+        });
+
+        // поток 2
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for( int i = 0; i < 10000; i++ ) {
+                    increment();
+                    //counter++;
+                    // аналоги
+                    //counter = counter + 1;
+                    //counter += 1;
+                }
+            }
+        });
+
+        // запуск 2х потоков
+        // инкримитируем counter и каждый раз разные значения
+        thread1.start();
+        thread2.start();
+        // ждем пока отработают 2 потока (один поток ждет др поток пока отработает)
+        thread1.join();
+        thread2.join();
+
+        System.out.println(counter);
+
+    }
+
+}
