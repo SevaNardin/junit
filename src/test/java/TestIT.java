94edbb0ac0decc78226a001fbd40491c2a8bb6cb
seva <vnardin@yandex.ru>
2020-11-15 10:38:28 +0300

    Добавил уроки

--- src/test/java/TestIT.java
+++ src/test/java/TestIT.java
@@ -1,60 +1,69 @@
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.concurrent.Executor;
-import java.util.logging.Logger;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /** Класс для интеграционных тестов */
 
 public class TestIT {
-    static Logger LOGGER;
-    private Response response;
+    // поле в области памяти
+    int a ;
+    int b ;
+    String str = "Test" ;
 
+    @BeforeEach
+    void init() {
+        a = 2;
+        b = 4;
+    }
+
+    @AfterEach
+    void finish() {
+        System.out.println( "Test Passed" + str );
+    }
 
     @Test
-    @DisplayName( "Пример теста" )
+    @DisplayName( "Теста 01" )
     void test() {
-        int a = 2 ;
-        int b = 4 ;
         int c = b / a ;
         // проверка что a = 2
-        Assertions.assertEquals( 2, a , "Error: no correct value!");
+        Assertions.assertEquals( 2, c , "Error: no correct value!");
     }
 
     @Test
-    @DisplayName( "Запуск потоков" )
-    void changeState() {
-        try {
-
-            Runnable task = () -> {
-                System.out.println("start");
-
-                response = RestAssured.get( "https://api.github.com/users/eugenp" );
-
-                System.out.println("stop");
-            };
-
-//            Thread thread1 = new Thread(task);
-//            Thread thread2 = new Thread(task);
-//            thread1.start();
-//            thread2.start();
-
-//            ExecutorService ex = Executors.newFixedThreadPool(2);
-//            ex.execute( task );
-
-            Executor executor = (runnable) -> {
-                new Thread(runnable).start();
-            };
-            executor.execute(task);
-            executor.execute(task);
-
-        } catch( Exception e  ) {
-            Assertions.fail(e);
-        }
-
+    @DisplayName( "Теста 02" )
+    void test2() {
+        int c = b / a ;
+        // проверка что a = 2
+        Assertions.assertEquals( 2, c , "Error: no correct value!");
     }
+
+
+
+
+
+
+
+
+    @ParameterizedTest( name = "#{index} - Run test with args={0}" )
+    @ValueSource( ints = { 1, 2, 3 } )
+    void test01( int num ) {
+        // передаваемое значение
+        System.out.println("Значение: " + num );
+        // объявляем итговое значение
+        int total = 1 + num ;
+
+        System.out.println("Итоговая сумма: " + total );
+        // проверяем что итоговое значение больше 2
+        assertTrue( total > 2 , "Errore: total than less 2!");
+    }
+
+    @ParameterizedTest(name = "#{index} - Run test with args={0}")
+    @ValueSource(ints = {1, 2, 3})
+    void test_int_arrays_custom_name(int arg) {
+        assertTrue(arg > 0);
+    }
+
 
 }
