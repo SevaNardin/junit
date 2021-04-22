9fddda62b38746777110ac1b085319fed9b751db
seva <vnardin@yandex.ru>
2021-04-22 11:10:02 +0300

    Добавил Cucumber BDD тесты

--- src/test/java/RunCucumberTest.java
+++ src/test/java/RunCucumberTest.java
@@ -3,7 +3,8 @@ import io.cucumber.junit.platform.engine.Cucumber;
 /**
  * @author Seva Nardin
  * Запуск тестов с Cocumber - BDD тесты
- */
+ * папка features для запуска сценария
+ * */
 
 @Cucumber
 public class RunCucumberTest {
