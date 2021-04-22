f8974946e3c28f245c2e718594cedc7abdf24723
seva <vnardin@yandex.ru>
2021-04-22 11:07:31 +0300

    Добавил Cucumber BDD тесты

---
+++ src/main/java/config/UserConfig.java
@@ -0,0 +1,12 @@
+package config;
+
+/**
+ * @author Seva Nardin
+ * Конфигурация для тестов Cucumber
+ */
+public class UserConfig {
+
+    public static final String USER_LOGIN = "test@test.com";
+    public static final String USER_PASSWORD = "Password!1";
+
+}
