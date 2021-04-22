1d9248b71b3a658fc576e6b8849ab817e53a612b
seva <vnardin@yandex.ru>
2021-04-22 11:07:15 +0300

    Добавил Cucumber BDD тесты

---
+++ src/main/java/pages/BasicPage.java
@@ -0,0 +1,24 @@
+package pages;
+
+import com.codeborne.selenide.Condition;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+
+/**
+ * @author Seva Nardin
+ */
+public class BasicPage {
+
+    public void clickButton(String text) {
+        $(By.xpath("//div[text()='" + text + "']")).click();
+    }
+
+    public void clickButtonSpan(String text) {
+        $(By.xpath("//span[text()='" + text + "']/..")).click();
+    }
+
+    public void contentIsVisible(String text) {
+        $(By.xpath("//*[text()='" + text + "']")).shouldBe(Condition.visible);
+    }
+}
